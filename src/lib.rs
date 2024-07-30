f04dc73e015c693291cdb5bf89e1b07ee5657740
Simon Cornelius <simonjoecornelius@gmail.com>
2024-07-30 19:57:37 +0200

    replace empty loop with hlt loop

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,8 @@ pub mod gdt;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
-
 use core::panic::PanicInfo;
+use x86_64;
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -55,7 +55,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    hlt_loop();
 }
 
 pub fn init() {
@@ -65,13 +65,19 @@ pub fn init() {
     x86_64::instructions::interrupts::enable();
 }
 
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
+
 /// Entry point for `cargo test`
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
